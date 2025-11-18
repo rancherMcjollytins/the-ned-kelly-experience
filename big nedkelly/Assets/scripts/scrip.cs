41677ef5eaa58a3d803b8785782a0083f402c6b7
adrian <adrian.uy@education.nsw.gov.au>
2025-11-18 11:11:03 +1100

    BONESMASH
    NEW LOOKSMAXXING TECHNIQUE: HAMMER YOURSELF

--- big nedkelly/Assets/scripts/scrip.cs
+++ big nedkelly/Assets/scripts/scrip.cs
@@ -8,13 +8,14 @@ public class scrip : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        lookaround = new Vector3(0, Input.GetAxis("Mouse X"), 0) * 90 * Time.deltaTime;
+        lookaround = new Vector3(0, Input.GetAxis("Mouse X"), 0) * 200 * Time.deltaTime;
         playerbody.transform.Rotate(lookaround);
     }
 }
