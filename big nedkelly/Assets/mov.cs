787744f5be468ea96b7be7bbe0986eb6cb73e0b2
adrian <adrian.uy@education.nsw.gov.au>
2025-11-24 12:31:28 +1100

    push 2 men

--- big nedkelly/Assets/mov.cs
+++ big nedkelly/Assets/mov.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class mov : MonoBehaviour
 {
+    public GameObject playerbody;
+    public InputActionAsset playerinputs;
+    private InputAction movementAction;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        movementAction = playerinputs.FindAction("Move");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 movement = movementAction.ReadValue<Vector3>() * Time.deltaTime;
+
     }
 }
